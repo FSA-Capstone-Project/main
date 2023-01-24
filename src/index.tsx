a184604d33864f58cfe57148acdc6211c4c6023a
James Tepper <jamesatepper@gmail.com>
2023-01-24 11:31:02 -0500

    added structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 import store from "./store";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -11,11 +10,11 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <BrowserRouter>
+  <Router>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </Router>
 );
 
 export default root;
