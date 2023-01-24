f37ca0339599a064033d4c26dd2ef482c911cd7a
James Tepper <jamesatepper@gmail.com>
2023-01-24 11:20:59 -0500

    added store

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import store from './app/store.ts';
-import ReactDOM from 'react-dom/client';
-import AppRoutes from './AppRoutes';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "./app/store.ts";
+import ReactDOM from "react-dom/client";
+import AppRoutes from "./AppRoutes";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-    <AppRoutes />
+      <AppRoutes />
     </Provider>
   </BrowserRouter>
 );
