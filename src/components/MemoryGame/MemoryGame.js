f879a3f28d1abf5f99f1e8977125932062a0585b
James Tepper <jamesatepper@gmail.com>
2023-01-31 15:16:50 -0500

    implemented memory game on home page

--- src/components/MemoryGame/MemoryGame.js
+++ src/components/MemoryGame/MemoryGame.js
@@ -1,38 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Box, Button, Grid } from "@mui/material/";
 
-// keep track of level => increment level if success
-// end if failure
-// keep track of inputs in level
-// add pause between tiles and add sequence of tiles
 
-//How the game functions
-// 1. User clicks start
-// 2. Game starts
-// 3. Game randomly selects a box and pings it before reverting it back to blue
-// 4. User clicks on the box that was pinged
-// 5. If the user clicks the correct box, the game moves on to the next level (level is incremented by 1)
-
-// Start game
-// Randomly select a box and ping it
-// Add the box to the order
-// Add the box to the ping order
-// Wait for user to click on the box
-// If the user clicks the correct box, increment the level by 1
-// If the user clicks the incorrect box, end the game
-// If the user clicks the correct box, ping the next box in the order
-
-//Todo have array indexed by level -1
 const MemoryGame = () => {
-  // When started: create a random box => ping random box
-  // Wait for click => if click === random box
-  // Increment level by one, choose random number and put it in sequence
-  // Increment variable by 1 for every CORRECT CLICK and reset it to 0 for every LEVEL INCREASE => this will be used to KEEP TRACK OF CURRENT CLICK
-
-  // Keep track of
-  // Game start
-  // The specific click
-  // The order of pings
 
   const [order, setOrder] = useState([]);
   const [clicks, setClicks] = useState(0);
@@ -51,8 +21,8 @@ const MemoryGame = () => {
     alert("Game Over");
     setGameStarted(false);
     setClicks(0);
-    setLevel(1);
     setOrder([]);
+    setLevel(1);
   };
 
   const clickedBox = (e) => {
