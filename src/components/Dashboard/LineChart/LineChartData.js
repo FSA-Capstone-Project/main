8a7eecc135610ab8b0c401a154a4f4157a6b50f2
hchalin <hchalin98@gmail.com>
2023-02-01 12:22:32 -0500

    added graph w/ fake data

---
+++ src/components/Dashboard/LineChart/LineChartData.js
@@ -0,0 +1,274 @@
+const data = [
+  {
+    "id": "japan",
+    "color": "hsl(133, 70%, 50%)",
+    "data": [
+      {
+        "x": "plane",
+        "y": 10
+      },
+      {
+        "x": "helicopter",
+        "y": 0
+      },
+      {
+        "x": "boat",
+        "y": 29
+      },
+      {
+        "x": "train",
+        "y": 143
+      },
+      {
+        "x": "subway",
+        "y": 218
+      },
+      {
+        "x": "bus",
+        "y": 155
+      },
+      {
+        "x": "car",
+        "y": 162
+      },
+      {
+        "x": "moto",
+        "y": 160
+      },
+      {
+        "x": "bicycle",
+        "y": 119
+      },
+      {
+        "x": "horse",
+        "y": 217
+      },
+      {
+        "x": "skateboard",
+        "y": 148
+      },
+      {
+        "x": "others",
+        "y": 296
+      }
+    ]
+  },
+  {
+    "id": "france",
+    "color": "hsl(52, 70%, 50%)",
+    "data": [
+      {
+        "x": "plane",
+        "y": 192
+      },
+      {
+        "x": "helicopter",
+        "y": 157
+      },
+      {
+        "x": "boat",
+        "y": 84
+      },
+      {
+        "x": "train",
+        "y": 210
+      },
+      {
+        "x": "subway",
+        "y": 71
+      },
+      {
+        "x": "bus",
+        "y": 14
+      },
+      {
+        "x": "car",
+        "y": 108
+      },
+      {
+        "x": "moto",
+        "y": 6
+      },
+      {
+        "x": "bicycle",
+        "y": 64
+      },
+      {
+        "x": "horse",
+        "y": 158
+      },
+      {
+        "x": "skateboard",
+        "y": 203
+      },
+      {
+        "x": "others",
+        "y": 284
+      }
+    ]
+  },
+  {
+    "id": "us",
+    "color": "hsl(69, 70%, 50%)",
+    "data": [
+      {
+        "x": "plane",
+        "y": 300
+      },
+      {
+        "x": "helicopter",
+        "y": 91
+      },
+      {
+        "x": "boat",
+        "y": 152
+      },
+      {
+        "x": "train",
+        "y": 115
+      },
+      {
+        "x": "subway",
+        "y": 60
+      },
+      {
+        "x": "bus",
+        "y": 237
+      },
+      {
+        "x": "car",
+        "y": 47
+      },
+      {
+        "x": "moto",
+        "y": 261
+      },
+      {
+        "x": "bicycle",
+        "y": 89
+      },
+      {
+        "x": "horse",
+        "y": 190
+      },
+      {
+        "x": "skateboard",
+        "y": 98
+      },
+      {
+        "x": "others",
+        "y": 195
+      }
+    ]
+  },
+  {
+    "id": "germany",
+    "color": "hsl(199, 70%, 50%)",
+    "data": [
+      {
+        "x": "plane",
+        "y": 77
+      },
+      {
+        "x": "helicopter",
+        "y": 287
+      },
+      {
+        "x": "boat",
+        "y": 117
+      },
+      {
+        "x": "train",
+        "y": 229
+      },
+      {
+        "x": "subway",
+        "y": 162
+      },
+      {
+        "x": "bus",
+        "y": 145
+      },
+      {
+        "x": "car",
+        "y": 55
+      },
+      {
+        "x": "moto",
+        "y": 44
+      },
+      {
+        "x": "bicycle",
+        "y": 151
+      },
+      {
+        "x": "horse",
+        "y": 81
+      },
+      {
+        "x": "skateboard",
+        "y": 134
+      },
+      {
+        "x": "others",
+        "y": 283
+      }
+    ]
+  },
+  {
+    "id": "norway",
+    "color": "hsl(138, 70%, 50%)",
+    "data": [
+      {
+        "x": "plane",
+        "y": 4
+      },
+      {
+        "x": "helicopter",
+        "y": 299
+      },
+      {
+        "x": "boat",
+        "y": 226
+      },
+      {
+        "x": "train",
+        "y": 145
+      },
+      {
+        "x": "subway",
+        "y": 116
+      },
+      {
+        "x": "bus",
+        "y": 119
+      },
+      {
+        "x": "car",
+        "y": 10
+      },
+      {
+        "x": "moto",
+        "y": 137
+      },
+      {
+        "x": "bicycle",
+        "y": 21
+      },
+      {
+        "x": "horse",
+        "y": 262
+      },
+      {
+        "x": "skateboard",
+        "y": 139
+      },
+      {
+        "x": "others",
+        "y": 184
+      }
+    ]
+  }
+]
+
+export default data
--- src/components/Dashboard/TrackedApps.jsx
+++ src/components/Dashboard/TrackedApps.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Typography, Grid, Box } from "@mui/material";
+import LineChart from "./LineChart/LineChart";
+import data from "./LineChart/LineChartData";
 
 function TrackedApps() {
   return (
     <Box>
       <Grid container justifyContent='space-around' spacing={2} xs={12} md={10} margin="auto" bgcolor='purple'>
         <Grid item xs={6}   bgcolor="#16161a" borderRadius="15px" height='300px'>
-          <Typography variant="h3">Some Graph</Typography>
-          <Typography variant="darktext">
-            {` Wow look at you douchbag. ChatGpt `}
-          </Typography>
+          <LineChart data={data}/>
         </Grid>
         <Grid item  xs={4}  bgcolor="#16161a" borderRadius="15px" height='300px'>
           <Typography variant="h3">Some other Graph</Typography>
