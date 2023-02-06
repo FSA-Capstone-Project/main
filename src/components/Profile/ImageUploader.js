02b53858dbd1ef1ef4071b6fac5bb978e84d0b4c
joe-leahy <josleahy@icloud.com>
2023-02-06 17:26:20 -0500

    profile pictures are working, upload in Profile tab

---
+++ src/components/Profile/ImageUploader.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { auth, app, storage } from "../../firebase";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { updateProfile } from "firebase/auth";
+
+const ImageUploader = () => {
+
+    const user = auth.currentUser
+
+    const [image, setImage] = useState(null);
+    const [url, setUrl] = useState(null);
+
+    const handleImageChange = (e) => {
+        if (e.target.files[0]) {
+          setImage(e.target.files[0]);
+        }
+      };
+    
+      const handleSubmit = () => {
+        const imageRef = ref(storage, "image");
+        uploadBytes(imageRef, image)
+          .then(() => {
+            getDownloadURL(imageRef)
+              .then((url) => {
+                setUrl(url);
+                console.log("URL", url)
+                updateProfile(user, {
+                    photoURL: `${url}`
+                  })
+              })
+              .catch((error) => {
+                console.log(error.message, "error getting the image url");
+              });
+            setImage(null);
+          })
+          .catch((error) => {
+            console.log(error.message);
+          });
+      };
+
+
+return (
+
+<div className="App">
+      <input type="file" onChange={handleImageChange} />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+)
+}
+
+export default ImageUploader
